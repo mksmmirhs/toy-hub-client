f935f46cff073a53f93d800aae449c960d096a24
Merajul Islam <meraj.exe@gmail.com>
2023-05-18 20:27:27 +0600

    footer added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from '../components/Shared/NavBar/NavBar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/Shared/Footer/Footer';
 
 const Main = () => {
   return (
     <div className=" container mx-auto">
       <NavBar></NavBar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
